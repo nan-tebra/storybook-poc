aa02f0be68216bc68f1e893dc3d747e6afc2d624
Nicole An <nicole.an@kareo.com>
2023-11-28 09:19:50 -0800

    small change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Storybook POC
         </a>
       </header>
     </div>
